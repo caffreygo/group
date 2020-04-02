7584d1db0dc7e6d55179efdea1c2ee2fba4c9b91
Jinrui Chen <caffreygo@163.com>
2020-04-02 15:49:53 +0800

    first

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  base: "/group/",
+  title: "Group Sources",
+  description: "Just playing around",
+  themeConfig: {
+    nav: [{ text: "Html", link: "/html/" }],
+    sidebar: {
+      "/html/": [""]
+    },
+    sidebarDepth: 2,
+    lastUpdated: "Last Updated"
+  }
+};
